1f04ee8f02ff0a9adc66b3d1063d04cd2286e8ba
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-09-10 12:02:12 -0300

    container criado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
+import Container from "./styles/container"
+import GlobalStyle from "./styles/global"
+import {ToastContainer , toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 
 export const app = () => {
     return (
-        <div className="app">
+        <>
+            <Container>
 
-        </div>
+            </Container>
+            <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+            <GlobalStyle/>
+        </>
     )
 }
 
