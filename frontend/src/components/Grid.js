0faf2a30d02996ad4ad766b945bb13387fccb0ed
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-09-11 12:24:05 -0300

    Grid criado

---
+++ frontend/src/components/Grid.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components';
+
+const Table = styled.table`
+    width: 100%;
+    
+`
+
+export function Grid() {
+  return (
+    <>
+
+    </>
+  );
+}
