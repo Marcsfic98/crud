3e01bcdca2586e554a080f507bc415b2459b5663
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-09-24 23:13:18 -0300

    feat: criada função get

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -3,6 +3,7 @@ import {db} from "../db.js";
 export const getUsers = (_,res)=> {
     const q = "SELECT * FROM users";
 
+    
     db.query(q, (err,data) => {
         if(err) return res.json(err);
 
